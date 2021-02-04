332bd53754a67890e9ee409cd26014ef86cccd3e
Masao Takaku <tmasao@acm.org>
2021-02-04 09:36:31 +0900

    use base_dir for the input file as well as original pdf files.

--- ext-pdfs.rb
+++ ext-pdfs.rb
@@ -10,13 +10,18 @@ end
 
 output = CombinePDF.new
 xlsx = Roo::Excelx.new(ARGV[0])
+base_dir = File.dirname(ARGV[0])
 xlsx.each_row_streaming(pad_cells: true) do |row|
   subject_id = row[2].value
   if subject_id.to_s =~ /^[0-9A-Z]/
-    p subject_id
     subject_name = row[3].value
     filename = row[13]&.value
     if filename and not filename.to_s.empty?
+      filename = File.join(base_dir, filename) if not File.exist? filename
+      if not File.exist? filename
+        puts "WARN: #{subject_id} not found: #{filename}"
+        next
+      end
       page_offset = row[14].value
       page_length = row[15].value
       pdf = CombinePDF.load(filename)
